478b151617ec876088cf4aff699737e43302ec0b
socrastein <31764357+socrastein@users.noreply.github.com>
2025-09-09 22:24:19 -0600

    Move grouping logic out of TipStore, set tipState to private variable

--- src/scripts/TipStore.ts
+++ src/scripts/TipStore.ts
@@ -14,45 +14,29 @@ export const TipStore = {
   }),
 
   findGroupByKey: function (key: string) {
-    let group = this.tipState.groupedTips.find(
+    let group = this._tipState.groupedTips.find(
       (group: any) => group.period === key
     );
     // Returns undefined if no group found
     return group;
   },
 
-  createGroupForTip: function (tip: Tip) {
-    const key = TipGrouper.getGroupKey(tip.date);
-    const group = reactive({ period: key, tips: [], total: 0 });
-
+  insertGroupInOrder: function (group: {
+    period: string;
+    tips: Tip[];
+    total: number;
+  }) {
     // See if there's a group with a period later than current group
-    const index = this.tipState.groupedTips.findIndex(
-      (group: any) => group.period > key
+    const index = this._tipState.groupedTips.findIndex(
+      (existingGroup: any) => existingGroup.period > group.period
     );
     // If none found, group represents most recent period so push to start of array
     if (index === -1) {
-      this.tipState.groupedTips.unshift(group);
+      this._tipState.groupedTips.unshift(group);
       // Place group into array just before first group period that's later than it
     } else {
-      this.tipState.groupedTips.splice(index, 0, group);
+      this._tipState.groupedTips.splice(index, 0, group);
     }
-
-    return group;
-  },
-
-  placeTipInGroup: function (
-    tip: Tip,
-    group: { period: string; tips: Tip[]; total: number }
-  ) {
-    const tipIndex = group.tips.findIndex(
-      (existingTip: Tip) => existingTip.date < tip.date
-    );
-    if (tipIndex === -1) {
-      group.tips.push(tip);
-    } else {
-      group.tips.splice(tipIndex, 0, tip);
-    }
-    group.total += tip.amount;
   },
 
   removeTipFromGroup: function (
@@ -65,36 +49,43 @@ export const TipStore = {
       return;
     }
     group.tips.splice(tipIndex, 1);
-    group.total -= tip.amount;
+    group.total = TipGrouper.calculateGroupTotal(group.tips);
+
     // Remove group if empty
     if (group.tips.length === 0) {
-      this.tipState.groupedTips.splice(
-        this.tipState.groupedTips.indexOf(group),
+      this._tipState.groupedTips.splice(
+        this._tipState.groupedTips.indexOf(group),
         1
       );
     }
   },
 
-  addTip: function (newTip: Tip) {
-    this.tipState.allTips.push(newTip);
-    const key = TipGrouper.getGroupKey(newTip.date);
-    let group = this.findGroupByKey(key);
-    if (!group) {
-      group = this.createGroupForTip(newTip);
-    }
-    this.placeTipInGroup(newTip, group);
+  addTip: function (tip: Tip) {
+    this._tipState.allTips.push(tip);
 
-    TipRepository.saveToStorage(newTip);
+    const key = TipGrouper.getGroupKey(tip.date);
+    let group = this.findGroupByKey(key);
+
+    if (!group) {
+      group = TipGrouper.createEmptyGroup;
+      this.insertGroupInOrder(group);
+    }
+
+    const insertIndex = TipGrouper.findInsertionIndex(group.tips, tip);
+    group.tips.splice(insertIndex, 0, tip);
+    group.total = TipGrouper.calculateGroupTotal(group.tips);
+
+    TipRepository.saveToStorage(tip);
   },
 
   removeTip: function (tip: Tip) {
     // Find and remove from allTips
-    const allTipsIndex = this.tipState.allTips.indexOf(tip);
+    const allTipsIndex = this._tipState.allTips.indexOf(tip);
     if (allTipsIndex === -1) {
       console.error(`Could not find tip ${tip} in allTips`);
       return;
     }
-    this.tipState.allTips.splice(allTipsIndex, 1);
+    this._tipState.allTips.splice(allTipsIndex, 1);
 
     // Find and remove from groupedTips
     const key = TipGrouper.getGroupKey(tip.date);
@@ -113,11 +104,12 @@ export const TipStore = {
       new Tip(newValues.amount, newValues.date, newValues.type, newValues.shift)
     );
     this.addTip(newTip);
+    return newTip;
   },
 
   checkForDuplicateTip: function (date: string, shift: string): Tip | null {
     return (
-      this.tipState.allTips.find(
+      this._tipState.allTips.find(
         (tip: Tip) => tip.date === date && tip.shift === shift
       ) || null
     );
