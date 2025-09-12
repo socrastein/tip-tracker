94e366b8c4819fa0baa93eef312c2f2eba4d73d1
socrastein <31764357+socrastein@users.noreply.github.com>
2025-09-12 15:53:46 -0600

    Create template for setting up new test scripts

---
+++ src/tests/modules/TEMPLATE.test.js
@@ -0,0 +1,37 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+
+beforeEach(() => {
+    // set constants before each test
+    // reset values, clear arrays, etc. 
+});
+
+const consoleSpy = vi.spyOn(console, 'log');
+
+
+describe('fileName', () => {
+
+    describe('methodBeingTested', () => {
+
+        it('should do X', () => {
+            expect(() => {/* function to return something, throw error, etc. */ }).not.toThrow();
+            expect(/* some variable to be a certain value */).toEqual();
+        });
+
+        it('should do Y', () => {
+
+        });
+
+    });
+
+    describe('methodBeingTested', () => {
+
+        it('should do X', () => {
+
+        });
+
+        it('should do Y', () => {
+
+        });
+
+    });
+});
